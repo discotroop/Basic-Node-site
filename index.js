b931d63e911750be7d012bd061252c0bfc96aade
John Mark Goodson <john.mark.hat@gmail.com>
2020-02-05 18:32:23 -0500

    converted basic node site to basic express site, mostly just used app.get();

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+// const http = require('http');
+// const url = require('url');
+// const fs = require('fs');
 
+/* changed to express */
+const express = require('express');
+const app = express();
 
-http.createServer(function(req, res) {
-    let q = url.parse(req.url, true);
-    let filename 
-        if (q.pathname === '/') {
-            filename = './index.html';
-        } else {
-            filename = '.' + q.pathname;
-        }
-    fs.readFile(filename, function(err, data) {
-        if (err) {
-            console.log("help")
-            const newPath = './404.html';
-            fs.readFile(newPath, function(err, data) {
-                res.writeHead(404, {'Content-type': 'text/html'});
-                res.write(data);
-                return res.end();
-            })
-        } else {
-        res.writeHead(200, {'Content-type': 'text/html'});
-        res.write(data);
-        return res.end();
-        }
-    });
-}).listen(8080)
+app.get('/', function(req, res) {
+    res.sendFile('./index.html', { root: __dirname});
+});
+app.get('/about', function(req, res) {
+    res.sendFile('./about.html', { root: __dirname});
+});
+app.get('/contact-me', function(req, res) {
+    res.sendFile('./contact-me.html', { root: __dirname});
+});
+app.get('*', function(req, res) {
+    res.sendFile('./404.html', { root: __dirname});
+});
+
+app.listen(3000, function() {
+    console.log("is it working?");
+});
+
+/* prior node version */
+
+// http.createServer(function(req, res) {
+//     let q = url.parse(req.url, true);
+//     let filename 
+//         if (q.pathname === '/') {
+//             filename = './index.html';
+//         } else {
+//             filename = '.' + q.pathname;
+//         }
+//     fs.readFile(filename, function(err, data) {
+//         if (err) {
+//             console.log("help")
+//             const newPath = './404.html';
+//             fs.readFile(newPath, function(err, data) {
+//                 res.writeHead(404, {'Content-type': 'text/html'});
+//                 res.write(data);
+//                 return res.end();
+//             })
+//         } else {
+//         res.writeHead(200, {'Content-type': 'text/html'});
+//         res.write(data);
+//         return res.end();
+//         }
+//     });
+// }).listen(8080)
